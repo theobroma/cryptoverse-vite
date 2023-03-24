dc2ec6f38b50795a11cbc993036412bae42492b6
Alexandr Siryj <theobroma222@gmail.com>
2023-03-24 22:39:27 +0200

    env example

--- src/shared/api/connection.ts
+++ src/shared/api/connection.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 // const API_HOST = process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST;
 
 export const API_URL = 'https://coinranking1.p.rapidapi.com';
 const API_HOST = 'coinranking1.p.rapidapi.com';
-const API_KEY = 'xxxxxxxxxxxxxx-xxxxxxxxxxx-xxxxxxxxxxxxxx';
+// const API_KEY = 'xxxxxxxxxxxxxx-xxxxxxxxxxx-xxxxxxxxxxxxxx';
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
