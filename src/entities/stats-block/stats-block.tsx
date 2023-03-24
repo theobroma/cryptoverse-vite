66527375652835552e865778b1445b9c7dac22d3
Alexandr Siryj <theobroma222@gmail.com>
2023-03-24 23:02:00 +0200

    stats blocks

---
+++ src/entities/stats-block/stats-block.tsx
@@ -0,0 +1,25 @@
+// https://mui.com/system/getting-started/the-sx-prop/#Example.tsx
+import { Box } from '@mui/material';
+
+interface Props {
+  title: string;
+  value: string | number;
+}
+
+export const StatsBlock = ({ title, value }: Props) => (
+  <Box
+    sx={{
+      bgcolor: 'background.paper',
+      boxShadow: 1,
+      borderRadius: 2,
+      px: 2,
+      py: 4,
+      minWidth: '100%',
+    }}
+  >
+    <Box sx={{ color: 'text.primary' }}>{title}</Box>
+    <Box sx={{ color: 'secondary.main', fontSize: 34, fontWeight: 'medium' }}>
+      {value}
+    </Box>
+  </Box>
+);
--- src/pages/home/home-global-stats/home-global-stats.tsx
+++ src/pages/home/home-global-stats/home-global-stats.tsx
@@ -1,3 +1,8 @@
+import millify from 'millify';
+
+import { Box, Grid, Typography } from '@mui/material';
+import { StatsBlock } from '../../../entities/stats-block/stats-block';
+import { AppError } from '../../../shared/uikit/app-error/app-error';
 import { useGetCryptosQuery } from '../../../store/coins/api';
 
 export const HomeGlobalStats = () => {
@@ -8,8 +13,62 @@ export const HomeGlobalStats = () => {
   console.log('globalStats :>> ', globalStats);
 
   return (
-    <div>
-      <span>home-global-stats</span>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {/* Title */}
+        <Grid item xs={12}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+            <Typography component="h2" variant="h5">
+              Global Crypto Stats
+            </Typography>
+          </Box>
+        </Grid>
+        {!!isSuccess && (
+          <>
+            {/* 1 */}
+            <Grid item xs={12} md={12}>
+              <StatsBlock
+                title="Total Cryptocurrencies"
+                value={millify(globalStats.total)}
+              />
+            </Grid>
+            {/* 2 */}
+            <Grid item xs={12} md={6}>
+              <StatsBlock
+                title=" Total Exchanges"
+                value={millify(globalStats.totalExchanges)}
+              />
+            </Grid>
+            {/* 3 */}
+            <Grid item xs={12} md={6}>
+              <StatsBlock
+                title="Total Market Cap:"
+                value={`$${millify(Number(globalStats.totalMarketCap))}`}
+              />
+            </Grid>
+            {/* 4 */}
+            <Grid item xs={12} md={6}>
+              <StatsBlock
+                title="Total 24h Volume"
+                value={`$${millify(Number(globalStats.total24hVolume))}`}
+              />
+            </Grid>
+            {/* 5  */}
+            <Grid item xs={12} md={6}>
+              <StatsBlock
+                title="Total Markets"
+                value={millify(globalStats.totalMarkets)}
+              />
+            </Grid>
+          </>
+        )}
+        {/* error */}
+        {!!isError && (
+          <Grid item xs={12}>
+            <AppError error={error} />
+          </Grid>
+        )}
+      </Grid>
+    </Box>
   );
 };
